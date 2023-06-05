14b3b1f6f9e39a2977676943218a837daf4ba7bf
GurianovDmitriy <gurianov6897@mail.ru>
2023-06-06 01:41:13 +0300

    4

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from api import API
-from middleware import Middleware
+
 
 app = API()
 
@@ -26,22 +26,6 @@ def sum(request, response, num_1, num_2):
     total = int(num_1) + int(num_2)
     response.text = f"{num_1} + {num_2} = {total}"
 
-@app.route("/exception")
-def exception_throwing_handler(request, response):
-    raise AssertionError("This handler should not be used.")
-
-@app.route("/template")
-def template_handler(req, resp):
-    resp.html = app.template("index.html", context={"name": "Bumbo", "title": "Best Framework"})
-
-@app.route("/json")
-def json_handler(req, resp):
-    resp.json = {"name": "data", "type": "JSON"}
-
-@app.route("/text")
-def text_handler(req, resp):
-    resp.text = "This is a simple text"
-
 @app.route("/book")
 class BooksResource:
     def get(self, req, resp):
@@ -49,22 +33,3 @@ class BooksResource:
 
     def post(self, req, resp):
         resp.text = "Endpoint to create a book"
-
-def handler(req, resp):
-    resp.text = "sample"
-
-app.add_route("/sample", handler)
-
-def custom_exception_handler(request, response, exception_cls):
-    response.text = str(exception_cls)
-
-app.add_exception_handler(custom_exception_handler)
-
-class SimpleCustomMiddleware(Middleware):
-    def process_request(self, req):
-        print("Processing request", req.url)
-
-    def process_response(self, req, res):
-        print("Processing response", req.url)
-
-app.add_middleware(SimpleCustomMiddleware)
